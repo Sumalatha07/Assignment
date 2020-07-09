2b771a5cd250c00ca2a076d7d5bcb0c1faf76eaf
Sumalatha Poojary <mesumalatha07@gmail.com>
2020-07-10 03:40:31 +0530

    distance

--- src/components/screens/UserLocation.js
+++ src/components/screens/UserLocation.js
@@ -64,12 +64,17 @@ export default class UserLocation extends Component{
             console.log("position",position);
             this.setState({lastLocationLatitude:position.coords.latitude, lastLocationLongitude: position.coords.longitude });
 
-            this.distance(this.state.latitude,this.state.longitude, this.state.lastLocationLatitude,  this.state.lastLocationLongitude, 'M')
+            var dist = this.distance(this.state.latitude,this.state.longitude, this.state.lastLocationLatitude,  this.state.lastLocationLongitude, 'M')
+            console.log("dist", dist);
+
+            if(dist>10){
+                this.sendNotification();
+            }
          });
       }
   
 
-      distance = (lat1, lon1, lat2, lon2, unit) => {
+      distance1234 = (lat1, lon1, lat2, lon2, unit) => {
         var radlat1 = Math.PI * lat1/180
         var radlat2 = Math.PI * lat2/180
         var theta = lon1-lon2
@@ -82,6 +87,21 @@ export default class UserLocation extends Component{
         // if (unit=="M") { dist = dist * 0.8684 }
         console.log('dist', dist)
         return dist
+    }
+
+    distance = (lat1,lon1,lat2,lon2) => {
+        var R = 6371; // km (change this constant to get miles)
+        var dLat = (lat2-lat1) * Math.PI / 180;
+        var dLon = (lon2-lon1) * Math.PI / 180;
+        var a = Math.sin(dLat/2) * Math.sin(dLat/2) +
+            Math.cos(lat1 * Math.PI / 180 ) * Math.cos(lat2 * Math.PI / 180 ) *
+            Math.sin(dLon/2) * Math.sin(dLon/2);
+        var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+        var d = R * c;
+        // if (d>1) return Math.round(d)+"km";
+        // else if (d<=1) return Math.round(d*1000)+"m";
+        var d = d * 1000;
+        return d;
     }
 
 
